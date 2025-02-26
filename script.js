892383b38a121e69e55a2f143c8e5f172a2223b6
nathiely chaves <161589375+nathielychaves@users.noreply.github.com>
2025-02-26 11:44:08 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+  let numero = prompt ("Digite um número");  
+ 
+  if (numero >= 10 && numero <= 20) {
+    console.log("O número está entre 10 e 20.");
+  } else {
+    console.log("O número não está entre 10 e 20.");
+  }
